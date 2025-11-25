a589a997d2101e5f63feeda6a3c3da6a2428b884
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-11-25 16:54:52 +0530

    C++ Working Open and Close Door (Tested)

--- Source/UE_BlueprintsToCpp/CppDoor.cpp
+++ Source/UE_BlueprintsToCpp/CppDoor.cpp
@@ -25,12 +25,30 @@ void ACppDoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (ShouldOpenOrClose) {
+		FRotator CurrentRotation = GetActorRotation();
+		FRotator NewRotation;
+		NewRotation.Roll = CurrentRotation.Roll;
+		NewRotation.Pitch = CurrentRotation.Pitch;
+		NewRotation.Yaw = CurrentRotation.Yaw + YawIncrement;
+		SetActorRotation(NewRotation);
+
+		if ((YawIncrement < 0 && NewRotation.Yaw <= -90) || (YawIncrement > 0 &&  NewRotation.Yaw >= 90)) {
+			ShouldOpenOrClose = false;
+		}
+	}
 }
 
 void ACppDoor::OpenDoor() {
-
+	if (GetActorRotation().Yaw > -90) {
+		YawIncrement = -2.0f;
+		ShouldOpenOrClose = true;
+	}
 }
 
 void ACppDoor::CloseDoor() {
-
+	if (GetActorRotation().Yaw < 90) {
+		YawIncrement = 2.0f;
+		ShouldOpenOrClose = true;
+	}
 }
