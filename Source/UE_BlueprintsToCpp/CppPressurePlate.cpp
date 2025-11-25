53aa68d240259bb8c4b926e8202bab43e8e2db72
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-11-25 20:06:17 +0530

    Add C++ implementation for Pressure Plate actor with collision component

---
+++ Source/UE_BlueprintsToCpp/CppPressurePlate.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppPressurePlate.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ACppPressurePlate::ACppPressurePlate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	PlateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlateMesh"));
+	PlateMesh->SetupAttachment(RootComponent);
+
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	SphereCollision->SetupAttachment(RootComponent);
+	SphereCollision->SetSphereRadius(100.0f);
+}
+
+// Called when the game starts or when spawned
+void ACppPressurePlate::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACppPressurePlate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UE_BlueprintsToCpp/CppPressurePlate.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppPressurePlate.generated.h"
+
+
+UCLASS()
+class UE_BLUEPRINTSTOCPP_API ACppPressurePlate : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* PlateMesh;
+
+	USphereComponent* SphereCollision;
+	// Sets default values for this actor's properties
+	ACppPressurePlate();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
