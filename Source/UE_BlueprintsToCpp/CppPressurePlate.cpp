0e5dd03e4ee8801d19ccebf9d20e0d19676f4979
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-11-25 21:55:17 +0530

    Created floating functionality by implementing core logic in C++, exposing it to Blueprint, and adding extra features in Blueprint.

--- Source/UE_BlueprintsToCpp/CppPressurePlate.cpp
+++ Source/UE_BlueprintsToCpp/CppPressurePlate.cpp
@@ -38,7 +38,7 @@ void ACppPressurePlate::Tick(float DeltaTime)
 
 }
 
-void ACppPressurePlate::OnPressurePlatePressed(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+void ACppPressurePlate::OnPressurePlatePressed_Implementation(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 	bool bFromSweep, const FHitResult& SweepResult)
 {
@@ -48,7 +48,7 @@ void ACppPressurePlate::OnPressurePlatePressed(UPrimitiveComponent* OverlappedCo
 	}
 }
 
-void ACppPressurePlate::OnPressurePlateReleased(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+void ACppPressurePlate::OnPressurePlateReleased_Implementation(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	if (OtherActor->IsA(AUE_BlueprintsToCppCharacter::StaticClass()) && DoorRef) {
--- Source/UE_BlueprintsToCpp/CppPressurePlate.h
+++ Source/UE_BlueprintsToCpp/CppPressurePlate.h
@@ -34,12 +34,12 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UFUNCTION()
+	UFUNCTION(BlueprintNativeEvent)
 	void OnPressurePlatePressed(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 		bool bFromSweep, const FHitResult& SweepResult);
 	
-	UFUNCTION()
+	UFUNCTION(BlueprintNativeEvent)
 	void OnPressurePlateReleased(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
